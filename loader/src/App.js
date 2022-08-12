2f434353978fc1f461376fe601f54a1ef460b2c3
Aleyna Celik <clkkaley@gmail.com>
2022-08-12 12:05:53 +0300

    useState useEffect Hooks added

--- loader/src/App.js
+++ loader/src/App.js
@@ -1,8 +1,31 @@
-
+import React,{useState,useEffect} from 'react'
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 function App() {
+  const [loading,setLoading]=useState(false);
+
+  //ilk render da çalışıcak
+  useEffect(()=>{
+    console.log("ilk render");
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },2500)
+  },[])
+
+
   return (
    <>
+   <div className="container">
+   
+      {
+        loading ?  
+        <div className="app">
+          <PropagateLoader color={"purple"} loading={loading} size={15} />
+        </div>
+          :    <div className='app'>Hoş Geldiniz nerdeydiniz ?</div>
+      }
+   </div>
    </>
   );
 }
