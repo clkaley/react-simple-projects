3c3ff810680b465fbdb493c7f9e4aab23f57afd3
Aleyna Celik <clkkaley@gmail.com>
2022-08-12 11:42:22 +0300

    added useState Hook

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,8 +1,20 @@
-
-
+import React,{useState} from "react"
+import images from "./images";
 function App() {
+
+  const[selectedImg,setSelectedImg]=useState(images[0]);
   return (
     <>
+    <h1>Photo Gallery</h1>
+    <div className="container">
+      <img src={selectedImg} alt="" />
+  
+    </div>
+    <div className="img-container">
+    {images.map((img,i)=>(
+            <img onClick={()=>setSelectedImg(img)} key={i} src={img} alt="gallery" />
+        ))}
+    </div>
     </>
   );
 }
---
+++ photo-gallery/src/images.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    "https://picsum.photos/id/238/200/300",
+    "https://picsum.photos/id/240/200/300",
+    "https://picsum.photos/id/23/200/300",
+    "https://picsum.photos/id/27/200/300",
+    "https://picsum.photos/id/37/200/300",
+    "https://picsum.photos/id/247/200/300",
+    "https://picsum.photos/id/57/200/300"
+]
