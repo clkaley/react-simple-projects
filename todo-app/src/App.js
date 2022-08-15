edaf0eada46ef86c984a9bfedee4e80694215a2f
Aleyna Celik <clkkaley@gmail.com>
2022-08-15 17:59:31 +0300

    add clock

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,33 @@
+import React,{useState,useEffect}from 'react'
+
 
 function App() {
+  const name="Aleyna"
+  let time=new Date().toLocaleTimeString();
+  const [currentTime,setCurrentTime]=useState(time);
+
+  const Time=()=>{
+    let time=new Date().toLocaleTimeString();
+    setCurrentTime(time)
+  }
+
+  setInterval(Time,1000);
+
   return (
     <>
+    <div>
+    <div className='clock'>
+      {currentTime}
+    </div>
+    <div className='container'>
+      <h1>{name}'s Todo App</h1>
+  
+    </div>
+    <div className='todos'>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+    </div>
     </>
   );
 }
