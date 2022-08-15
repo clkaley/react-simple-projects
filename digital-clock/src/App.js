68002b74c763aa120651929f7af61fd14bfdb170
Aleyna Celik <clkkaley@gmail.com>
2022-08-15 12:30:29 +0300

    add currentTime function

--- digital-clock/src/App.js
+++ digital-clock/src/App.js
@@ -1,7 +1,24 @@
+import React,{useState} from 'react'
+
 
 function App() {
+  let time=new Date().toLocaleTimeString();
+
+  const[currentTime,setCurrentTime]=useState(time);
+
+  const Time=()=>{
+    let time=new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+
+  setInterval(Time,1000);
+
   return (
     <>
+    <div className='container'>
+      <h1>Digital Clock</h1>
+      <div className='currentTime'>{currentTime}</div>
+    </div>
     </>
   );
 }
