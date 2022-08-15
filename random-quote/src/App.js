deac89a958f01b6c801a38b27bb13347f4780fe8
Aleyna Celik <clkkaley@gmail.com>
2022-08-15 22:26:44 +0300

    added fetch

--- random-quote/src/App.js
+++ random-quote/src/App.js
@@ -1,8 +1,19 @@
-
+import React,{useState,useEffect} from 'react'
 
 function App() {
+  const [quotes,setQuotes]=useState("");
+
+  const getQuote=()=>{
+    fetch("https://type.fit/api/quotes").
+    then((res)=>res.json()).
+    then((data)=>console.log(data[1]));
+  }
   return (
    <>
+   <div>
+    <button onClick={getQuote}>Get Quote</button>
+
+   </div>
    </>
   );
 }
