bcc5610c65b5cc7656a9291bf083a204eec1286d
Aleyna Celik <clkkaley@gmail.com>
2022-08-16 10:59:02 +0300

    added props

--- cards/src/App.js
+++ cards/src/App.js
@@ -4,7 +4,31 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-    <Card/>
+    <div>
+    <h1 className='title'>The Rick and Morty API</h1>
+    <div className="wrapper">
+    <Card
+      img="https://rickandmortyapi.com/api/character/avatar/48.jpeg"
+      name="Black Rick"
+    />
+     <Card
+      img="https://rickandmortyapi.com/api/character/avatar/54.jpeg"
+      name=" Bobby Moynihan"
+    />
+     <Card
+      img="https://rickandmortyapi.com/api/character/avatar/270.jpeg"
+      name="Prince Nebulon"
+    />
+      <Card
+      img="https://rickandmortyapi.com/api/character/avatar/392.jpeg"
+      name=" Bearded Morty"
+    />
+      <Card
+      img="https://rickandmortyapi.com/api/character/avatar/742.jpeg"
+      name="Bruce Chutback"
+    />
+    </div>
+    </div>
     </>
   );
 }
--- cards/src/components/Card.js
+++ cards/src/components/Card.js
@@ -1,74 +1,21 @@
 import React from 'react'
 
-function Card() {
- 
-
+function Card(props) {
+  console.log("props",props);
 
   return (
     <>
-    <h1 className='title'>The Rick and Morty API</h1>
-    <div className='wrapper'>
-      
         <div className="card">
             <div className="card-body">
-                <img src="https://rickandmortyapi.com/api/character/avatar/48.jpeg" alt="" />
+                <img src={props.img} alt="" />
                 <div className="about">
                     <h4 className='cardTitle'>
-                    Black Rick
+                   {props.name}
                     </h4>
                     <button>Details</button>
                 </div>
-            </div>
+            </div>   
         </div>
-
-        <div className="card">
-            <div className="card-body">
-                <img src="https://rickandmortyapi.com/api/character/avatar/54.jpeg" alt="" />
-                <div className="about">
-                    <h4 className='cardTitle'>
-                    Bobby Moynihan
-                    </h4>
-                    <button>Details</button>
-                </div>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="card-body">
-                <img src="https://rickandmortyapi.com/api/character/avatar/270.jpeg" alt="" />
-                <div className="about">
-                    <h4 className='cardTitle'>
-                    "Prince Nebulon
-                    </h4>
-                    <button>Details</button>
-                </div>
-            </div>
-        </div>
-        <div className="card">
-            <div className="card-body">
-                <img src="https://rickandmortyapi.com/api/character/avatar/392.jpeg" alt="" />
-                <div className="about">
-                    <h4 className='cardTitle'>
-                    Bearded Morty
-                    </h4>
-                    <button>Details</button>
-                </div>
-            </div>
-        </div>
-
-        <div className="card">
-            <div className="card-body">
-                <img src="https://rickandmortyapi.com/api/character/avatar/742.jpeg" alt="" />
-                <div className="about">
-                    <h4 className='cardTitle'>
-                    Bruce Chutback
-                    </h4>
-                    <button>Details</button>
-                </div>
-            </div>
-        </div>
-      
-    </div>
     </>
   )
 }
