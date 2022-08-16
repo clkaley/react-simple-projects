f86759c7d5f15a886aa17fe660badedef5989e4b
Aleyna Celik <clkkaley@gmail.com>
2022-08-16 13:48:20 +0300

    added axios

--- axios-test/src/App.js
+++ axios-test/src/App.js
@@ -1,10 +1,31 @@
-import React,{useEffect,useState} from'react'
-
+import React,{useEffect,useState} from'react';
+import axios from 'axios'
 function App() {
-  
+  const [users,setUsers]=useState([]);
 
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res=>setUsers(res.data));
+  },[])
+
+  
   return (
    <>
+   <div>
+    {
+        users.map((user,i)=>{
+          return (
+            <div key={i}>
+              <p>
+                {user.name}-
+                {user.username}
+              </p>
+
+            </div>
+          )
+        })
+    }
+   </div>
    </>
   );
 }
