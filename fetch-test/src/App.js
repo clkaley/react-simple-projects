5c67109c9beb5af6b4fd3473274e320774dd6ace
Aleyna Celik <clkkaley@gmail.com>
2022-08-16 12:58:39 +0300

    added useState, useEffectHooks and fetch

--- fetch-test/src/App.js
+++ fetch-test/src/App.js
@@ -1,8 +1,33 @@
-
+import React,{useState,useEffect} from 'react'
 
 function App() {
+  const[countries,setCountries]=useState([]);
+
+  
+  //! api dan veri almak asenkron bir iş
+  useEffect(()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(res=>setCountries(res))
+    },[])
+
+    console.log("countries:",countries,);
+
+
   return (
   <>
+  <div>
+    {
+      countries.map((country,i)=>{
+        return( 
+          <div key={i}>
+            {country.capital}
+            <img width={15} src={country.flags.png} alt="" />
+          </div>
+        )
+      })
+    }
+  </div>
   </>
   );
 }
