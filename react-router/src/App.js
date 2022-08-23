a38c48fef3f7a7d1b21ed7db1920e28dacd5d4fb
Aleyna Celik <clkkaley@gmail.com>
2022-08-23 19:09:07 +0300

    added configure routes but basic structure

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
    <>
+    <BrowserRouter>
+      <Routes>
+        {/* localhost:3000 "/" denktir.*/}
+        <Route path='/' element={<div>hello</div>}/>
+        <Route path='testing' element={<div>test component</div>}/>
+      </Routes>
+    </BrowserRouter>
    </>
   );
 }
