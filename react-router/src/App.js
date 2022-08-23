8d5a7c3c68e595b8169eddfb2a0840eadc44e638
Aleyna Celik <clkkaley@gmail.com>
2022-08-23 19:15:27 +0300

    created pages folder and components (home,about,product)

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -5,15 +5,18 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
 
 function App() {
   return (
    <>
     <BrowserRouter>
       <Routes>
-        {/* localhost:3000 "/" denktir.*/}
-        <Route path='/' element={<div>hello</div>}/>
-        <Route path='testing' element={<div>test component</div>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='product' element={<Product/>}/>
+        <Route path='about' element={<About/>}/>
       </Routes>
     </BrowserRouter>
    </>
