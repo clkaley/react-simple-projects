eec451ec5d7d73b771423f783177dd332917ecfd
Aleyna Celik <clkkaley@gmail.com>
2022-08-24 10:39:24 +0300

    changed navlink -> link

--- react-router/src/components/StyledNavbar/index.js
+++ react-router/src/components/StyledNavbar/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './index.css'
 
 function StyledNavbar() {
   return (
     <div>
         <nav className='navbar'>
-            <NavLink  to="/"
-            >Home</NavLink>
-            <NavLink className='btn' to="/about">About</NavLink>
-            <NavLink className='btn' to="/product">Product</NavLink>
+            <Link  to="/"
+            >Home</Link>
+            <Link className='btn' to="/about">About</Link>
+            <Link className='btn' to="/product">Product</Link>
         </nav>
     </div>
   )
