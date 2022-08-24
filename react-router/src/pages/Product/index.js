0ec7e87f9a6c934bb01024a1db1ff2704bbb50b0
Aleyna Celik <clkkaley@gmail.com>
2022-08-24 11:59:25 +0300

    created data.js connected single product pages

--- react-router/src/pages/Product/index.js
+++ react-router/src/pages/Product/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../Product/index.css'
+import products from '../../data'
 function Product() {
   return (
     <div>
-    
-      <h3>Product</h3>
+      <h3>Products</h3>
+      <div className='products'>
+        {products.map((product)=>{
+          return (
+            <div key={product.id}>
+              <h1>{product.name}</h1>
+              <Link to={`/product/${product.id}`} >More Info</Link>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- react-router/src/pages/SingleProduct/index.js
+++ react-router/src/pages/SingleProduct/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import './index.css'
+import products  from '../../data'
+
 function SingleProduct() {
    const {productID}=useParams();
+
+   const product=products.find((product)=>product.id===productID)
+
+   console.log(product);
+   const{image, name}=product
   return (
     <>
     <div className='container'>Single Product
+    <h4>{name}</h4>
+    <img src={image} alt={name} />
     </div>
     <Link to='/product' className="btn">Back To Product</Link>
     </>
