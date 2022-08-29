f5bec034a8f671315d33937365d808010d222ff1
Aleyna Celik <clkkaley@gmail.com>
2022-08-29 14:10:32 +0300

    added react-query-devtools

--- react-query-fetch/src/index.js
+++ react-query-fetch/src/index.js
@@ -5,14 +5,23 @@ import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnMount:false,
+      refetchOnWindowFocus:false
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </>
 );
