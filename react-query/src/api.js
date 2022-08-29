f3960fe7576e8557ed8548cb18e510d557a081ee
Aleyna Celik <clkkaley@gmail.com>
2022-08-29 18:37:19 +0300

    created .env file because to reduce code duplication

--- react-query/src/api.js
+++ react-query/src/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
 export const fetchUsers=async()=>{
-    const {data}=await axios.get('https://jsonplaceholder.typicode.com/users')
+    const {data}=await axios.get(`
+    ${process.env.REACT_APP_BASE_ENDPOINT}/users
+    `)
     return data
 }
 
 
 export const fetchUserDetail=async(id)=>{
-    const {data}=await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const {data}=await axios.get(` ${process.env.REACT_APP_BASE_ENDPOINT}/users/${id}`)
     return data
 }
