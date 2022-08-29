a55f7a54513f157c155687a7b8b8125ca7863981
Aleyna Celik <clkkaley@gmail.com>
2022-08-29 15:19:11 +0300

    created pages and components folder

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,14 +1,18 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import User from "./pages/User";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="users" element={<User />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
