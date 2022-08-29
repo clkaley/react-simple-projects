5851299b31100074d7a5eacd4b8a4b0c534796f3
Aleyna Celik <clkkaley@gmail.com>
2022-08-29 13:02:34 +0300

    added provider, client and useQuery structure

--- react-query-fetch/src/App.js
+++ react-query-fetch/src/App.js
@@ -1,9 +1,29 @@
-
+import { useQuery } from "react-query";
 
 function App() {
+
+
+  const { isLoading, error, data } = useQuery("users", () =>
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json()
+    )
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log("data", data);
   return (
     <>
-    <h1>React Query</h1>
+      <h1 style={{ textAlign: "center" }}>React Query</h1>
+      {data.map((item, id) => {
+        return (
+          <div key={id}>
+            {item.name}-{item.username}
+          </div>
+        );
+      })}
     </>
   );
 }
