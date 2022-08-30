528fa38d0a9a613fac75fc3be0490f44cd748197
Aleyna Celik <clkkaley@gmail.com>
2022-08-30 10:42:47 +0300

    created pages and components structure and connected router

--- react-swr-router/src/App.js
+++ react-swr-router/src/App.js
@@ -1,9 +1,18 @@
-
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
 
 function App() {
   return (
    <>
-   hello
+  <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+      <Route path="*" element={<Error/>}/>
+  </Routes>   
    </>
   );
 }
