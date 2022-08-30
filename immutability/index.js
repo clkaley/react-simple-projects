6ac69ada9ce7eeaaff0ac7f8b3ea0eccee4dcbcb
Aleyna Celik <clkkaley@gmail.com>
2022-08-30 23:18:46 +0300

    example immutability

---
+++ immutability/index.js
@@ -0,0 +1,26 @@
+//Primitive Type
+// let name1="aleyna";
+//let name2="murat"
+// let name2=name1
+
+// console.log(name1);
+// console.log(name2);
+
+const user1={
+    name:"Mehmet",
+    isActive:true
+}
+
+//const user2=user1;
+//user 2 nin name inin değişmesini istesek bile farklı oluyo
+//user2.name="Aleyna"
+//!bunun olmaması için 1. yol const user2={...user1}; 2.yol const user2=Object.assign({},user1);
+//const user2={...user1};
+//const user2=Object.assign({},user1);
+user2.name="ali"
+
+
+console.log(user1)
+console.log(user2)
+
+
