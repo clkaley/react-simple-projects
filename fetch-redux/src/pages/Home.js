013ae8933a397c9603fec24a445028805665968f
Aleyna Celik <clkkaley@gmail.com>
2022-08-31 23:46:39 +0300

    created redux structure and charactersSlice.js file

--- fetch-redux/src/pages/Home.js
+++ fetch-redux/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 function Home() {
+  const data = useSelector((state) => state.characters)
+  console.log(data);
   return (
     <div>Home</div>
   )
