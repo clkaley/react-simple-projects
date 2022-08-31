27b668117853195d156ce71391951b4252be975a
Aleyna Celik <clkkaley@gmail.com>
2022-08-31 23:28:36 +0300

    added router structure

--- fetch-redux/src/App.js
+++ fetch-redux/src/App.js
@@ -1,10 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Characters from "./pages/Characters";
+import Episodes from "./pages/Episodes";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-   
-    </div>
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="characters" element={<Characters />}/>
+        <Route path="episodes" element={<Episodes />}/>
+      </Routes>
+    </>
   );
 }
 
