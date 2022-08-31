2ab6a86a0552456ee2a1d9e03f448e3908ccdd07
Aleyna Celik <clkkaley@gmail.com>
2022-08-31 17:45:54 +0300

    added dispatch

--- counter-app-redux/src/redux/components/Counter.js
+++ counter-app-redux/src/redux/components/Counter.js
@@ -1,16 +1,35 @@
-import React from "react";
-import { useSelector ,useDispatch} from "react-redux";
-import { decrement, increment } from "../counter/counterSlice";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  decrementByAmount
+} from "../counter/counterSlice";
 
 function Counter() {
   const value = useSelector((state) => state.counter.value);
-  const dispatch=useDispatch()
+  const [amount, setAmount] = useState( value);
+  const dispatch = useDispatch();
   console.log("counter value", value);
   return (
     <div style={{ textAlign: "center" }}>
       <h1>{value}</h1>
-      <button onClick={()=>dispatch(decrement())}>Decrement</button>
-      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <button className="btn btn-danger m-2" onClick={() => dispatch(decrement())}>Decrement</button>
+      <button  className="btn btn-success" onClick={() => dispatch(increment())}>Increment</button>
+
+      <br />
+      <br />
+      <input className="mt-3" type="number" value={amount} onChange={(e)=>setAmount(e.target.value)}
+        />
+      <button className="btn btn-success mt-2" onClick={() => dispatch(incrementByAmount(amount))}>
+        Increment by amount
+      </button>
+      <input className="mt-5" type="number" value={amount} onChange={(e)=>setAmount(e.target.value)}
+        />
+      <button className="btn btn-danger mt-2" onClick={() => dispatch(decrementByAmount(amount))}>
+        Decrement by amount
+      </button>
     </div>
   );
 }
--- counter-app-redux/src/redux/counter/counterSlice.js
+++ counter-app-redux/src/redux/counter/counterSlice.js
@@ -1,24 +1,33 @@
-//createSlice bize bir Slice oluşturucak bunun içinde ise state in içinde durucak her şey burda olucak
+//!createSlice bize bir Slice oluşturucak bunun içinde ise state in içinde durucak her şey burda olucak
 import { createSlice } from "@reduxjs/toolkit";
 
-//niye dışarı aktardık store.js üzerindeki reducers a vericez.
+//!niye dışarı aktardık store.js üzerindeki reducers a vericez.
 export const counterSlice = createSlice({
-  //state ulaşmak istediğimiz zaman state.counter
+  //!state ulaşmak istediğimiz zaman state.counter
   name: "counter",
   initialState: {
     value: 0,
   },
-  //update etmek için kullancağımız kısım reducers kısmı
+  //!update etmek için kullancağımız kısım reducers kısmı
   reducers: {
-    //state in içinde üstteki objemiz olucak yani value ya erişebiliriz.
+    //! state in içinde üstteki objemiz olucak yani value ya erişebiliriz.
     increment:(state)=>{
         state.value+=1;
     },
     decrement:(state)=>{
         state.value-=1;
+    },
+    //! action.payload componentden gelen değeri aktarıyor bize 
+    incrementByAmount:(state,action)=>{
+        console.log("action",action);
+        state.value+=Number(action.payload)
+    },
+    decrementByAmount:(state,action)=>{
+        console.log("action",action);
+        state.value-=Number(action.payload)
     }
   },
 });
 
-export const {increment,decrement}=counterSlice.actions
+export const {increment,decrement,incrementByAmount,decrementByAmount}=counterSlice.actions
 export default counterSlice.reducer;
