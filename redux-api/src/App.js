2f026e8da5deef1f2618b1dc138329194be33ad7
Aleyna Celik <clkkaley@gmail.com>
2022-09-26 22:10:37 +0300

    created route structure

--- redux-api/src/App.js
+++ redux-api/src/App.js
@@ -1,10 +1,20 @@
-
-
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Characters from "./pages/Characters";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div >
-   
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home />}/>
+      <Route path="characters" element={<Characters/>}  />
+  
+  </Routes>
+  </>
   );
 }
 
