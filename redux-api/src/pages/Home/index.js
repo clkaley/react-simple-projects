ce81b28c80c2372c2d0abcd5a112b83b85e67347
Aleyna Celik <clkkaley@gmail.com>
2022-10-06 17:34:28 +0300

    deleted some funct into the Home page

--- redux-api/src/pages/Home/index.js
+++ redux-api/src/pages/Home/index.js
@@ -1,27 +1,7 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchCharacters } from "../../redux/charactersSlice";
-import "../Home/style.css";
-import Loading from "../../components/Loading";
-import Error from "../../components/Error";
-import { Link } from "react-router-dom";
+import React from "react";
+
 function Home() {
-  const characters = useSelector((state) => state.characters.items);
-  const isLoading = useSelector((state) => state.characters.isLoading);
-  const error = useSelector((state) => state.characters.error);
-  console.log(characters);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCharacters());
-  }, [dispatch]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <Error />;
-  }
   return (
     <div>
       <div className="container mt-5">
