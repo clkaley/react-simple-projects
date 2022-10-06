9a3bedf705fc59ab516e383b14737debcc11bb9c
Aleyna Celik <clkkaley@gmail.com>
2022-10-06 21:11:10 +0300

    added QuotesDetail page

---
+++ redux-api/src/pages/QuotesDetail/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function QuotesDetail() {
+  const { quotesID } = useParams();
+  console.log("quotesID", quotesID);
+  const items = useSelector((state)=>state.quotes.items.find((item) => item.quote_id === Number(quotesID)));
+
+  if(!items){
+    return <Navigate to="/quotes"/>
+  }
+  console.log(items);
+
+  return (
+    <div>
+      <h1 className="text-center mt-5">QUOTE DETAIL</h1>
+      <figure className="text-center">
+        <blockquote className="blockquote">
+          <p>{items.quote}</p>
+        </blockquote>
+        <figcaption className="blockquote-footer">
+          {items.author} <cite title="Source Title">{items.series} - {items.quote_id} </cite>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+
+export default QuotesDetail;
