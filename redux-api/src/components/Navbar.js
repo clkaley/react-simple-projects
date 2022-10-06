766c9b0c0f0b1f5039e5aff0cc06e872785935cb
Aleyna Celik <clkkaley@gmail.com>
2022-10-06 21:10:42 +0300

    changed smth

--- redux-api/src/components/Navbar.js
+++ redux-api/src/components/Navbar.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import './style.css'
+import "./style.css";
 function Navbar() {
   return (
-  <>
-  <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">BD</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-link active" aria-current="page" to="/">Breaking Bad</Link>
-        <Link className="nav-link" to="/character">Characters</Link>
-        <Link className="nav-link" to="/quotes">Quotes</Link>
-       
-      </div>
-    </div>
-  </div>
-</nav>
-  </>
-  )
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+          Breaking Bad
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/character">
+                Characters
+              </Link>
+              <Link className="nav-link" to="/quotes">
+                Quotes
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
