81bdb02a97b0d7d839a1a4073ab567447a17334b
Aleyna Celik <clkkaley@gmail.com>
2022-10-06 21:23:16 +0300

    added spinner into the Loading Component

--- redux-api/src/components/Loading.js
+++ redux-api/src/components/Loading.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import PropagateLoader   from "react-spinners/PropagateLoader";
 function Loading() {
   return (
-    <div style={{padding:"10px",color:"red"}}>Loading...</div>
+    <div className='text-center mt-5'>
+   <PropagateLoader color={"red"} size={15} />
+    </div>
   )
 }
 
